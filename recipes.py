482c50fd17dd0de2428e4306734766f6a2139258
Денис <denis@MacBook-Air-Denis.local>
2026-06-03 02:20:17 +0300

    DietaryRecipe is ready

--- recipes.py
+++ recipes.py
@@ -61,3 +61,12 @@ class Recipe:
             return s[:-1]
         else:
             return ""
+class DietaryRecipe(Recipe):
+    def __init__(self,title,diet_type,ingredients=None):
+        super().__init__(title,ingredients)
+        self.diet_type = diet_type
+    def scale(self,ratio):
+        n = super().scale(ratio)
+        return DietaryRecipe(self.title,self.diet_type,n.ingredients)
+    def __str__(self):
+        return f"({self.diet_type}) {super().__str__()}"
