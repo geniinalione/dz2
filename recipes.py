c783b4f21813750718a03c5c694ec0b2b8e9d59a
Денис <denis@MacBook-Air-Denis.local>
2026-06-03 02:10:35 +0300

    Recipe is complete

--- recipes.py
+++ recipes.py
@@ -20,3 +20,44 @@ class Ingredient:
         if self.unit == other.unit and self.name == other.name:
             return True
         return False
+    
+class Recipe:
+    def __init__(self,title,ingredients=None):
+        self.title = title
+        if ingredients is not None:
+            self.ingredients = ingredients
+        else:
+            self.ingredients = []
+
+    def add_ingredient(self,ingredient):
+        for line in self.ingredients:
+            if line == ingredient:
+                line.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        if type(ratio) == int or type(ratio) == float:
+            if ratio > 0:
+                return True
+        return False
+    
+    def scale(self,ratio):
+        if self.is_valid_ratio(ratio) == False:
+            raise ValueError("Неправильный ratio")
+        new_ingredients = []
+        for i in self.ingredients:
+            ni = Ingredient(i.name,i.quantity*ratio,i.unit)
+            new_ingredients.append(ni)
+        return Recipe(self.title,new_ingredients)
+    def __len__(self):
+        return(len(self.ingredients))
+    def __str__(self):
+        s = self.title + ": "
+        for i in self.ingredients:
+            s += f"{i.name},"
+        if len(self.ingredients) > 0:
+            return s[:-1]
+        else:
+            return ""
